9d7af614126e32b3b71633f06931af2d1e61fc3f
Gboyega Daniel <119269023+GboyegaAkere@users.noreply.github.com>
2025-02-19 18:01:45 +0100

    setting up the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collections from './pages/Collections'
+import About from './pages/About'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Order from './pages/Order'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <div className='text-red-600'>
-      hell
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/collection' element={<Collections/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/product/:productId' element={<Product/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/place-order' element={<PlaceOrder/>}/>
+          <Route path='/order' element={<Order/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
     </div>
   )
 }
