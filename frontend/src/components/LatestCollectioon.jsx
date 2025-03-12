b8d915409335808bf596968456b597a76bd1a173
Gboyega Daniel <119269023+GboyegaAkere@users.noreply.github.com>
2025-03-12 19:36:50 +0100

    best seller section

--- frontend/src/components/LatestCollectioon.jsx
+++ frontend/src/components/LatestCollectioon.jsx
@@ -1,22 +1,32 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
+import ProductItems from './ProductItems'
 
 const LatestCollectioon = () => {
-    const {products} = useContext(ShopContext)
-    console.log(products)
+    const { products } = useContext(ShopContext);
+    const [latestProducts, setLatestProducts] = useState([]);
 
-    
-  return (
-    <div className='my-10'>
-      <div className='text-center py-8 text-3xl'>
-       <Title text1={'LATEST'} text2={"COLLECTIONS"}/>
-       <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-        This is our lastest collections, feel freee to place an order from the collections       
-       </p>
-      </div>
-    </div>
-  )
-}
+    useEffect(() => {
+        setLatestProducts(products.slice(0, 10));
+    }, []);
 
-export default LatestCollectioon
+    return (
+        <div className='my-10'>
+            <div className='text-center py-8 text-3xl'>
+                <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                    This is our latest collection. Feel free to place an order from the collection.
+                </p>
+            </div>
+
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
+                {latestProducts.map((item, index) => (
+                    <ProductItems key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default LatestCollectioon;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollectioon from '../components/LatestCollectioon'
+import BestSeller from '../components/BestSeller'
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
          <Hero/>
          <LatestCollectioon/>
+         <BestSeller/>
     </div>
   )
 }
