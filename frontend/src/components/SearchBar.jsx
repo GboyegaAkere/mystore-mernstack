aea3599c0511371cec344492ac73f8474846b1a2
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2025-05-07 16:45:35 +0100

    added serch input

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,14 +1,19 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets'
 
 const SearchBar = () => {
     const {search,setSearch,showSearch,setShowSearch} =useContext(ShopContext)
     
-  return (
-    <div>
-      <h1>heloo</h1>
-    </div>
-  )
+    return showSearch ? (
+      <div className='border-t border-5 bg-gray-50 text-center'>
+          <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2 '>
+              <input value={search} onChange={(e)=>setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text-sm' type="text" placeholder='Search' name="" id="" /> 
+              <img src={assets.search_icon} alt="" className='w-4' />
+          </div>
+          <img onClick={()=>setShowSearch(false)} src={assets.cross_icon} className='inline w-3 cursor-pointer' alt="" />
+      </div>
+    ) : null
 }
 
 export default SearchBar
