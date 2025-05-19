99707c34c9f1a1e1201af5bbd1c026bc1c1f84b8
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2025-05-19 14:13:46 +0100

    working on the product details page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext'
 
 const Product = () => {
-  return (
+const {productId} = useParams()
+const {products} = useContext(ShopContext)
+const [productsData, setProductsData] = useState(false)
+const [image, setImage] = useState('')
+
+// console.log(productId)
+
+const fetchProductsData = async () => {
+  products.map((item)=>{
+    if (item._id === productId) {
+      setProductsData(item)
+      setImage(item.image[0])
+      console.log(item)
+      return null
+    }
+  })
+}
+
+useEffect(()=>{
+  fetchProductsData()
+},[productId,products])
+
+
+  return productsData ? (
     <div>Product</div>
-  )
+  ) : <div></div>
 }
 
 export default Product
