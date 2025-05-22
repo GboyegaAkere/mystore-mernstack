c76b6897857269ce49264ad7060ee8a542621809
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2025-05-22 09:42:49 +0100

    worked on the product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,14 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets'
 
 const Product = () => {
 const {productId} = useParams()
-const {products} = useContext(ShopContext)
+const {products,currency,price} = useContext(ShopContext)
 const [productsData, setProductsData] = useState(false)
 const [image, setImage] = useState('')
+const [size, setSize] = useState('')
 
-// console.log(productId)
+//  console.log(productId)
 
 const fetchProductsData = async () => {
   products.map((item)=>{
@@ -40,13 +42,49 @@ useEffect(()=>{
               ))
             }
           </div>
+
+          {/* ------------- product image ----------- */}
+
           <div className='w-full sm:w-[80%]'>
             <img className='w-full h-auto' src={image} alt="" />
           </div>
         </div>
+
+        {/* --------------product informations----------- */}
+            <div className='flex-1'>
+              <h1 className='font-medium text-2xl mt-2'>{productsData.name}</h1>
+              <div className='flex items-center gap-1 mt-2'>
+                <img src={assets.star_icon} alt="" className='w-3.5'/>
+                <img src={assets.star_icon} alt="" className='w-3.5'/>
+                <img src={assets.star_icon} alt="" className='w-3.5'/>
+                <img src={assets.star_icon} alt="" className='w-3.5'/>
+                <img src={assets.star_dull_icon} alt="" className='w-3.5'/>
+                <p className='pl-2'>{123}</p>
+              </div>
+              <p className='mt-5 text-3xl font-medium'>{currency}{productsData.price}</p>
+              <p className='mt-5 text-gray-500 md:w-4/5'>{productsData.description}</p>
+              <div className='flex flex-col gap-4 my-8'>
+                <p>Select Size</p>
+                <div className='flex gap-2'>
+                  {productsData.sizes.map((item,index)=>(
+                    <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? "border-orange-500" : ""}`} key={index}>{item}</button>
+                  ))}
+                </div>
+
+              </div>
+              <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+              <hr className='mt-8 sm:w-4/5'/>
+              <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                <p>100% Original product</p>
+                <p>Cash on delivery i available on the product</p>
+                <p>Easy return and exchange policy within 7days</p>
+
+              </div>
+            </div>
+
       </div>
     </div>
-  ) : <div></div>
+  ) : <div className='opacity-0'></div>
 }
 
 export default Product
