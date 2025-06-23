43488688d1913823d5ad26b98ff4944f2f57f59e
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2025-06-23 09:11:12 +0100

    new commit

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 
 
@@ -10,9 +10,33 @@ const ShopContextProvider =(props)=>{
     const deliveryFee = 10;
     const [search,setSearch] = useState('')
     const[showSearch, setShowSearch] = useState(false)
+    const [cartItems, setCartItems] = useState({});
+
+const addToCart = (itemId, size) => {
+  setCartItems(prev => {
+    const updatedCart = { ...prev };
+
+    // Initialize item if it doesn't exist
+    if (!updatedCart[itemId]) {
+      updatedCart[itemId] = {};
+    }
+
+    // Initialize or increment size quantity
+    updatedCart[itemId][size] = (updatedCart[itemId][size] || 0) + 1;
+
+    return updatedCart;
+  });
+};
+
+useEffect(() => {
+  console.log(cartItems);
+}, [cartItems]);
+
+
+
 
     const value = {
-        products,currency,deliveryFee,search,setSearch,showSearch,setShowSearch
+        products,currency,deliveryFee,search,setSearch,showSearch,setShowSearch,cartItems,addToCart
     }
 
     return (
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets'
+import RelatedProducts from '../components/RelatedProducts'
 
 const Product = () => {
 const {productId} = useParams()
-const {products,currency,price} = useContext(ShopContext)
+const {products,currency,addToCart,price} = useContext(ShopContext)
 const [productsData, setProductsData] = useState(false)
 const [image, setImage] = useState('')
 const [size, setSize] = useState('')
@@ -72,17 +73,31 @@ useEffect(()=>{
                 </div>
 
               </div>
-              <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+              <button onClick={()=>addToCart(productsData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
               <hr className='mt-8 sm:w-4/5'/>
               <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
                 <p>100% Original product</p>
                 <p>Cash on delivery i available on the product</p>
                 <p>Easy return and exchange policy within 7days</p>
-
               </div>
-            </div>
+            </div> 
+          </div>
 
-      </div>
+
+                {/* -------------Description $ Review Section ---------- */}
+          <div className='mt-20'>
+            <div className='flex'>
+              <b className='border px-5  py-3 text-sm'>Description</b>
+              <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+            </div>
+            <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel tempora sint obcaecati, recusandae delectus quam, porro quis minima quo dignissimos voluptates dicta, voluptas doloremque alias iure! Rerum magni esse dolor.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora corrupti voluptatibus inventore nostrum maxime deleniti commodi quibusdam iusto soluta dolore recusandae, at quisquam vel non ratione eligendi tenetur fuga laudantium!</p>
+            </div>
+          </div>
+
+              {/* ---------- Display related products ------------ */}
+         <RelatedProducts category={productsData.category} subcategory={productsData.subcategory}/>
     </div>
   ) : <div className='opacity-0'></div>
 }
