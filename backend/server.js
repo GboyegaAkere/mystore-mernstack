ddb4fa9cc8bbabccc4d613ff6f6bdb05067d7a3a
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2025-08-26 21:59:58 +0100

    settling up the app

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+//API ENDPOINT
+app.get("/",(req,res)=>{
+    res.send("API IS WORKING")
+})
+
+app.listen(port,()=>console.log('server started:' +port))
