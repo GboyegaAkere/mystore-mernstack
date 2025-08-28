5f7d617672207575c8bdce6136aa8e0e4d4b0920
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2025-08-28 16:01:36 +0100

    connect clodinary

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         console.log("DB connected")
     })
 
-    await mongoose.connect(`${process.env.MONGODB_URL}/e-commerce`)
+    await mongoose.connect(`${process.env.MONGODB_URL}/e-commerce `)
 }
 
 export default connectDB
